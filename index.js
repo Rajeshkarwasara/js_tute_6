92582f19050dfc5f626650af5f458dd391d8abfd
Rajesh karwasara <140477625+Rajeshkarwasara@users.noreply.github.com>
2023-08-20 21:10:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+let are=["ram","sita   "]
+document.write(are[1])
+
+let name="ram"
+document.write(name.charAt(1))
+
+document.write(name.length)
+
+let are1=[];
+are1[0]="sita";
+are1[1]="ram";
+document.write(are1)
+
+
+for(let raj=0; raj<=1000; raj++)
+{
+    document.write(" ",'rajesh'," ");
+}
+// for(let p=0; p=are.length; p++){
+//     document.write(are[p]);
+// }
+
+document.write(are.join("2"))
+
+// document.write(are)
+are.pop();
+
+
